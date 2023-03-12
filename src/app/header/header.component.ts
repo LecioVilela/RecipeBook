c5e0a12a376b76ccde626bdbf520c2f8d9bbc922
LecioVilela <lecio.vilela@yahoo.com>
2023-03-12 13:01:53 -0300

    add navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
 
   collapsed = true;
 
@@ -14,4 +14,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
